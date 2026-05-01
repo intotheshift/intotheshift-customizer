26e1e084460dd4c8a239636dd80ab1f042c9cdea
CaroleMeandyoutoo <54663864+CaroleMeandyoutoo@users.noreply.github.com>
2026-05-01 19:03:11 +0200

    Update header.js

--- header.js
+++ header.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.getElementById("header");
   if (!header) return;
+
   header.innerHTML = `
     <div class="topbar">
       <div class="logo" onclick="window.location.href='index-bibliotheque.html'">
-        <img src="into the shift fond blanc.png" alt="Into The Shift" class="logo-img">
+        <img src="into-the-shift-logo.png" alt="Into The Shift" class="logo-img">
       </div>
-      <div class="tb-sp"></div>
-      <div class="acct">
-        <div class="av">SR</div>
-        <span class="acct-n">Sonora RH</span>
+
+      <div class="tb-spacer"></div>
+
+      <div class="account-pill">
+        <div class="avatar">SR</div>
+        <span>Sonora RH</span>
       </div>
     </div>
   `;
